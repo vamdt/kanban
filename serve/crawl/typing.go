6b022016620f26f2f0a71498177a6a19ba872126
Dejian Xu <xudejian2008@gmail.com>
2015-10-16 10:27:49 +0800

    reconstruction ParseTyping as some funcs

--- serve/crawl/typing.go
+++ serve/crawl/typing.go
@@ -60,12 +60,75 @@ type typing_parser struct {
 	t Typing
 }
 
+func (p *Tdatas) clean_typing_parser() {
+	if len(p.tp) > 3 {
+		var tmp []typing_parser
+		tmp = append(tmp, p.tp[len(p.tp)-3:]...)
+		p.tp = tmp
+	}
+}
+
+func (p *Tdatas) new_typing_parser_node(i int) {
+	if len(p.tp) > 0 {
+		p.tp[len(p.tp)-1].t.End = i - 1
+	}
+	tp := typing_parser{}
+	tp.t.begin = i
+	tp.t.I = i
+	tp.t.End = i
+	tp.d = p.Data[i]
+	p.tp = append(p.tp, tp)
+}
+
+func (p *Tdatas) parse_top_bottom_typing() {
+	if len(p.tp) < 3 {
+		return
+	}
+	typing := p.tp[len(p.tp)-2].t
+	a := &p.tp[len(p.tp)-3].d
+	b := &p.tp[len(p.tp)-2].d
+	c := &p.tp[len(p.tp)-1].d
+	if IsTopTyping(a, b, c) {
+		typing.Price = b.High
+		typing.Type = TopTyping
+	} else if IsBottomTyping(a, b, c) {
+		typing.Price = b.Low
+		typing.Type = BottomTyping
+	} else {
+		return
+	}
+
+	typing.High = b.High
+	typing.Low = b.Low
+	typing.Time = b.Time
+
+	if len(p.Typing) > 0 {
+		if typing.I-p.Typing[len(p.Typing)-1].I < 4 {
+			return
+		}
+
+		if typing.Type == TopTyping && p.Typing[len(p.Typing)-1].Type == BottomTyping {
+			if typing.High <= p.Typing[len(p.Typing)-1].High {
+				return
+			}
+		}
+
+		if typing.Type == p.Typing[len(p.Typing)-1].Type {
+			if pos, ok := TypingSlice(p.Typing).MergeTyping(typing); ok {
+				if pos < len(p.Typing)-1 {
+					p.Typing = p.Typing[:pos+1]
+				}
+				return
+			}
+		}
+	}
+	p.Typing = append(p.Typing, typing)
+}
+
 func (p *Tdatas) ParseTyping() {
-	var prev *typing_parser
 	start := 0
 	if l := len(p.tp); l > 0 {
 		start = p.tp[l-1].t.End + 1
-		prev = &p.tp[l-1]
 	} else {
 		start = 0
 	}
@@ -74,42 +137,15 @@ func (p *Tdatas) ParseTyping() {
 		a := &p.Data[i]
 
 		if len(p.tp) < 1 {
-			tp := typing_parser{}
-			tp.t.begin = i
-			tp.t.I = i
-			tp.t.End = i
-			tp.d = p.Data[i]
-			tp.t.High = tp.d.High
-			tp.t.Low = tp.d.Low
-			tp.t.Time = tp.d.Time
-			p.tp = append(p.tp, tp)
-			prev = &p.tp[len(p.tp)-1]
+			p.new_typing_parser_node(i)
+			continue
 		}
 
+		prev := &p.tp[len(p.tp)-1]
 		if IsUpTyping(&prev.d, a) {
-			prev.t.End = i - 1
-			tp := typing_parser{}
-			tp.t.begin = i
-			tp.t.I = i
-			tp.t.End = i
-			tp.d = p.Data[i]
-			tp.t.High = tp.d.High
-			tp.t.Low = tp.d.Low
-			tp.t.Time = tp.d.Time
-			p.tp = append(p.tp, tp)
-			prev = &p.tp[len(p.tp)-1]
+			p.new_typing_parser_node(i)
 		} else if IsDownTyping(&prev.d, a) {
-			prev.t.End = i - 1
-			tp := typing_parser{}
-			tp.t.begin = i
-			tp.t.I = i
-			tp.t.End = i
-			tp.d = *a
-			tp.t.High = tp.d.High
-			tp.t.Low = tp.d.Low
-			tp.t.Time = tp.d.Time
-			p.tp = append(p.tp, tp)
-			prev = &p.tp[len(p.tp)-1]
+			p.new_typing_parser_node(i)
 		} else if Contain(&prev.d, a) {
 			var base *Tdata
 			if len(p.tp) > 1 {
@@ -121,69 +157,20 @@ func (p *Tdatas) ParseTyping() {
 			if IsUpTyping(base, &prev.d) {
 				if prev.d.High != a.High {
 					prev.t.I = i
-					prev.t.Time = p.Data[i].Time
 				}
 			} else if IsDownTyping(base, &prev.d) {
 				if prev.d.Low != a.Low {
 					prev.t.I = i
-					prev.t.Time = p.Data[i].Time
 				}
 			}
 			prev.d = *a
 			prev.t.End = i
-			prev.t.High = a.High
-			prev.t.Low = a.Low
 		} else {
 			log.Println("UnknowTyping", a)
 		}
 
-		if len(p.tp) > 3 {
-			var tmp []typing_parser
-			tmp = append(tmp, p.tp[len(p.tp)-3:]...)
-			p.tp = tmp
-			prev = &p.tp[len(p.tp)-1]
-		}
-
-		if len(p.tp) > 2 {
-			typing := p.tp[len(p.tp)-2].t
-			a := &p.tp[len(p.tp)-3].d
-			b := &p.tp[len(p.tp)-2].d
-			c := &p.tp[len(p.tp)-1].d
-			if IsTopTyping(a, b, c) {
-				typing.Price = b.High
-				typing.Type = TopTyping
-			} else if IsBottomTyping(a, b, c) {
-				typing.Price = b.Low
-				typing.Type = BottomTyping
-			} else {
-				continue
-			}
-
-			typing.High = b.High
-			typing.Low = b.Low
-
-			if len(p.Typing) > 0 {
-				if typing.I-p.Typing[len(p.Typing)-1].I < 4 {
-					continue
-				}
-
-				if typing.Type == TopTyping && p.Typing[len(p.Typing)-1].Type == BottomTyping {
-					if typing.High <= p.Typing[len(p.Typing)-1].High {
-						continue
-					}
-				}
-
-				if typing.Type == p.Typing[len(p.Typing)-1].Type {
-					if pos, ok := TypingSlice(p.Typing).MergeTyping(typing); ok {
-						if pos < len(p.Typing)-1 {
-							p.Typing = p.Typing[:pos+1]
-						}
-						continue
-					}
-				}
-			}
-			p.Typing = append(p.Typing, typing)
-		}
+		p.clean_typing_parser()
+		p.parse_top_bottom_typing()
 	}
 }
 
@@ -214,6 +201,7 @@ func ContainMerge(pra, a, b *Tdata) *Tdata {
 	if IsUpTyping(pra, a) {
 		if b.High > a.High {
 			t.High = b.High
+			t.Time = b.Time
 		}
 		if b.Low > a.Low {
 			t.Low = b.Low
@@ -222,6 +210,7 @@ func ContainMerge(pra, a, b *Tdata) *Tdata {
 	} else if IsDownTyping(pra, a) {
 		if b.Low < a.Low {
 			t.Low = b.Low
+			t.Time = b.Time
 		}
 		if b.High < a.High {
 			t.High = b.High
