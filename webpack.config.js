ceff9248ed415591bc255d8aa5a9fa835785b0f4
Dejian Xu <xudejian2008@gmail.com>
2015-11-10 16:03:37 +0800

    only webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 var path = require("path");
 var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
 	cache: true,
   entry: {
     main: "./app/scripts/main.coffee"
   },
 	output: {
-		path: path.join(__dirname, ".tmp/scripts"),
+		path: path.join(__dirname, ".tmp"),
 		filename: "[name].js",
 		chunkFilename: "[chunkhash].js"
 	},
   devtool: 'source-map',
   resolve: {
-    extensions: ['', '.coffee', '.js']
+    extensions: ['', '.coffee', '.js'],
+    modulesDirectories: ['node_modules', 'scripts', 'styles', 'images', 'fonts']
   },
 	module: {
+    noParse: /\.min\.js/,
 		loaders: [
-			// required to write "require('./style.css')"
-			{ test: /\.css$/,    loader: "style-loader!css-loader!autoprefixer-loader" },
+			{
+        test: /\.css$/,
+        //loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+      },
 
 			// required for bootstrap icons
 			{ test: /\.woff$/,   loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
@@ -25,10 +31,19 @@ module.exports = {
 			{ test: /\.eot$/,    loader: "file-loader?prefix=font/" },
 			{ test: /\.svg$/,    loader: "file-loader?prefix=font/" },
 
+      { test: /\.png$/, loader: "url-loader?mimetype=image/png" },
+      { test: /\.gif$/, loader: "file-loader" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.vue$/, loader: "vue" },
       { test: /\.coffee$/, loader: "coffee-loader?sourceMap" }
 		]
 	},
+  postcss: [
+    require('autoprefixer'),
+    require('postcss-color-rebeccapurple')
+  ],
 	plugins: [
+    //new ExtractTextPlugin("main.css"),
 		new webpack.ProvidePlugin({
 			// Automtically detect jQuery and $ as free var in modules
 			// and inject the jquery library
