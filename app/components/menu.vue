bb709a2c95fe00bec283b12ec0e78a8970d174c7
Dejian Xu <xudejian2008@gmail.com>
2015-12-07 17:34:16 +0800

    remove link from menu

--- app/components/menu.vue
+++ app/components/menu.vue
@@ -12,7 +12,7 @@
     <ul class="pure-menu-list">
         <li class="pure-menu-item pure-menu-selected"><a v-link="{ path: '/' }" class="pure-menu-link">Home</a></li>
         <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-            <a href="#" id="menuLink1" class="pure-menu-link">Stock</a>
+            <a class="pure-menu-link">Stock</a>
             <ul class="pure-menu-children">
                 <li class="pure-menu-item"><a v-link="{ path: '/s/sh600570/1' }" class="pure-menu-link">sh600570</a></li>
                 <li class="pure-menu-item"><a v-link="{ path: '/s/sh601005/1' }" class="pure-menu-link">sh601005</a></li>
