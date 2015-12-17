7f4b1d1422f03f4c99e791f06e6efe65e8f278c8
Dejian Xu <xudejian2008@gmail.com>
2015-12-17 17:26:05 +0800

    add ocl options

--- app/components/settings.vue
+++ app/components/settings.vue
@@ -3,13 +3,20 @@
     <fieldset>
       <div class="pure-control-group">
         <label for="nc" class="pure-checkbox">
-          <input id="nc" v-model="nc" @click="submit" type="checkbox"> no candle
+          <input id="nc" v-model="settings.nc" @click="submit" type="checkbox"> no candle
         </label>
       </div>
 
       <div class="pure-control-group">
         <label for="nmas" class="pure-checkbox">
-          <input id="nmas" v-model="nmas" @click="submit" type="checkbox"> no mas
+          <input id="nmas" v-model="settings.nmas" @click="submit" type="checkbox"> no mas
+        </label>
+      </div>
+
+      <div class="pure-control-group">
+        <label for="ocl" class="pure-checkbox">
+          <input id="ocl" v-model="settings.ocl" @click="submit" type="checkbox">
+          only candle line
         </label>
       </div>
     </fieldset>
@@ -20,16 +27,14 @@
 module.exports =
   route:
     data: ->
-      try
-        JSON.parse localStorage.getItem 'settings'
-      catch
-        nc: true
-        nmas: true
+      settings: try
+          JSON.parse localStorage.getItem 'settings'
+        catch
+          nc: true
+          nmas: true
+          ocl: true
 
   methods:
     submit: ->
-      settings =
-        nc: @nc
-        nmas: @nmas
-      localStorage.setItem('settings', JSON.stringify(settings))
+      localStorage.setItem('settings', JSON.stringify(@settings))
 </script>
