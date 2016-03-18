347306fcbfa6f1d94b22af72058101436528dfb3
Dejian Xu <xudejian2008@gmail.com>
2016-03-18 17:27:19 +0800

    imple model with merge assign

--- app/stock/model.js
+++ app/stock/model.js
@@ -1,21 +1,126 @@
 import EventEmitter from './event';
+import { bisector, time } from 'd3';
+
+const bisect = bisector((d) => +d.date);
+const parseDate = time.format('%Y-%m-%dT%XZ').parse;
+
+function merge(name, data, Clazz) {
+  if (!Array.isArray(data)) {
+    return;
+  }
+
+  if (data.length < 1) {
+    return;
+  }
+
+  const o = this[name];
+  if (o.length > 0) {
+    const ndate = +parseDate(data[0].time);
+    const odate = +o[o.length - 1].date;
+    const o0date = +o[0].date;
+    if (odate < ndate) {
+    } else if (o0date >= ndate) {
+      while(this[name].length) {
+        this[name].pop();
+      }
+    } else {
+      const i = bisect.left(o, ndate);
+      while(this[name].length > i) {
+        this[name].pop();
+      }
+    }
+  }
+
+  data.forEach((d) => {
+    this[name].push(new Clazz(name, d));
+  });
+}
+
+class Tdata {
+  constructor(name, prop) {
+    Object.defineProperty(this, 'name', {
+      value: 'k',
+    });
+
+    Object.keys(prop).forEach((p) => {
+      Object.defineProperty(this, p, {
+        enumerable: true,
+        value: prop[p],
+      });
+    });
+
+    Object.defineProperty(this, '_date', {
+      writable: true,
+    });
+
+    Object.defineProperty(this, 'date', {
+      get() {
+        this._date = this._date || parseDate(this.time);
+        return this._date;
+      },
+    });
+  }
+}
+
+class Typing {
+  constructor(name, prop) {
+    Object.defineProperty(this, 'name', {
+      value: name,
+    });
+
+    Object.keys(prop).forEach((p) => {
+      Object.defineProperty(this, p, {
+        enumerable: true,
+        value: prop[p],
+      });
+    });
+
+    Object.defineProperty(this, '_date', {
+      writable: true,
+    });
+
+    Object.defineProperty(this, 'date', {
+      get() {
+        this._date = this._date || parseDate(this.time);
+        return this._date;
+      },
+    });
+  }
+}
+
+const tsprops = [
+  'Data',
+  'Line',
+];
 
 class Typings {
   constructor(name) {
     Object.defineProperty(this, 'name', {
       value: name.toLowerCase(),
     });
-    const props = [
-      'Data',
-      'Line',
-    ];
-    props.forEach((name) => {
-      Object.defineProperty(this, name, {
+    tsprops.forEach((prop) => {
+      Object.defineProperty(this, prop, {
+        enumerable: true,
         value: [],
       });
     });
   }
+
+  assign(data) {
+    if (typeof data !== 'object') {
+      return;
+    }
+    tsprops.forEach((name) => {
+      merge.call(this, name, data[name], Typing);
+    });
+  }
 }
+
+const lprops = [
+  'Typing',
+  'Segment',
+  'Hub',
+];
 
 class Level {
   constructor(name, prev) {
@@ -30,21 +135,39 @@ class Level {
     }
 
     Object.defineProperty(this, 'data', {
+      enumerable: true,
       value: [],
     });
 
-    const props = [
-      'Typing',
-      'Segment',
-      'Hub',
-    ];
-    props.forEach((name) => {
-      Object.defineProperty(this, name, {
-        value: new Typings(name),
+    lprops.forEach((prop) => {
+      Object.defineProperty(this, prop, {
+        enumerable: true,
+        value: new Typings(prop),
       });
     });
   }
+
+  assign(data) {
+    if (typeof data !== 'object') {
+      return;
+    }
+
+    merge.call(this, 'data', data.data, Tdata);
+
+    lprops.forEach((name) => {
+      this[name].assign(data[name]);
+    });
+  }
 }
+
+const levels = [
+  'm1s',
+  'm5s',
+  'm30s',
+  'days',
+  'weeks',
+  'months',
+];
 
 export default class Model extends EventEmitter {
   constructor(id) {
@@ -58,20 +181,23 @@ export default class Model extends EventEmitter {
       writable: true,
     });
 
-    const levels = [
-      'm1s',
-      'm5s',
-      'm30s',
-      'days',
-      'weeks',
-      'months',
-    ];
-    let prev = false;
-    levels.forEach((lname) => {
-      Object.defineProperty(this, lname, {
-        value: new Level(lname, prev),
+    levels.reduce((prev, name) => {
+      Object.defineProperty(this, name, {
+        enumerable: true,
+        value: new Level(name, prev),
       });
-      prev = lname;
+      return name;
+    }, false);
+  }
+
+  assign(data) {
+    if (typeof data !== 'object' && this.id !== data.id) {
+      return;
+    }
+    this.name = data.Name;
+
+    levels.forEach((name) => {
+      this[name].assign(data[name]);
     });
   }
 }
--- test/model.test.js
+++ test/model.test.js
@@ -52,6 +52,7 @@ describe('Model', () => {
 
     describe('levels', () => {
       it('should has expect property', () => {
+        let prev = false;
         levels.forEach((k) => {
           Object.keys(def_level).forEach((p) => {
             assert.property(m[k], p);
@@ -61,6 +62,20 @@ describe('Model', () => {
               assert.typeOf(m[k][p], typeof def_level[p], `${k}.${p}`);
             }
           });
+          if (prev) {
+            assert.equal(m[k].prev, prev);
+          }
+          prev = k;
+        });
+      });
+
+      it('should has expect prev', () => {
+        let prev = false;
+        levels.forEach((k) => {
+          if (prev) {
+            assert.equal(m[k].prev, prev);
+          }
+          prev = k;
         });
       });
     });
@@ -72,6 +87,70 @@ describe('Model', () => {
       const m = new Model(id);
       assert.equal(m.id, id);
       assert.throws(() => m.id = '001', 'Cannot assign');
+    });
+  });
+
+  describe('assign', () => {
+    it('should assign .data currect', () => {
+      const m = new Model('007');
+      const o = {
+        m1s: {
+          data: [
+            { time: '2001-08-20T00:00:00Z' },
+          ],
+        },
+      };
+      m.assign(o);
+      assert.instanceOf(m.m1s.data[0].date, Date);
+      assert.equal(m.m1s.data[0].time, o.m1s.data[0].time);
+      assert.typeOf(m.m1s.data[0].time, 'string', 'data[].time should be a time string');
+      assert.lengthOf(m.m1s.data[0].time, 20, 'should be a time string with format %Y-%m-%dT%XZ');
+    });
+
+    describe('merge .data', () => {
+      it('should merge pop the repeat data', () => {
+        const m = new Model('007');
+        const o = {
+          m1s: {
+            data: [
+              { time: '2001-08-20T00:00:00Z' },
+            ],
+          },
+        };
+        m.assign(o);
+        m.assign(o);
+        assert.equal(m.m1s.data[0].time, o.m1s.data[0].time);
+        assert.lengthOf(m.m1s.data, 1);
+      });
+
+      it('should merge currect', () => {
+        const m = new Model('007');
+        const o = {
+          m1s: {
+            data: [
+              { time: '2001-08-20T00:00:00Z' },
+              { time: '2001-08-21T00:00:00Z' },
+              { time: '2001-08-22T00:00:00Z' },
+              { time: '2001-08-23T00:00:00Z' },
+            ],
+          },
+        };
+        const n = {
+          m1s: {
+            data: [
+              { time: '2001-08-21T00:00:00Z' },
+              { time: '2001-08-24T00:00:00Z' },
+            ],
+          },
+        };
+        m.assign(o);
+        assert.equal(m.m1s.data[0].time, o.m1s.data[0].time);
+        assert.lengthOf(m.m1s.data, o.m1s.data.length);
+        m.assign(n);
+        assert.equal(m.m1s.data[0].time, o.m1s.data[0].time);
+        assert.lengthOf(m.m1s.data, 3);
+        assert.equal(m.m1s.data[2].time, n.m1s.data[1].time);
+      });
     });
   });
 });
