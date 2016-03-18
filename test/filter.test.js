6f33f0ce1a2b3480350a80d4a274194e85293801
Dejian Xu <xudejian2008@gmail.com>
2016-03-18 18:39:10 +0800

    remove unuse func; fixes filter and test

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,3 +1,6 @@
+/* eslint-env mocha */
+/* global assert */
+
 import { filter } from '../app/stock/util';
 
 describe('Util', () => {
@@ -19,33 +22,33 @@ describe('Util', () => {
     it('should have one item before range[0] and after range[last]', () => {
       const src = [
         {
-          Time: '2001-08-20T00:00:00Z',
+          date: 20010820,
           ETime: '2001-08-20T01:00:00Z',
         },
         {
-          Time: '2001-08-21T00:00:00Z',
+          date: 20010821,
           ETime: '2001-08-21T01:00:00Z',
         },
         {
-          Time: '2001-08-22T00:00:00Z',
+          date: 20010822,
           ETime: '2001-08-22T01:00:00Z',
         },
         {
-          Time: '2001-08-23T00:00:00Z',
+          date: 20010823,
           ETime: '2001-08-23T01:00:00Z',
         },
         {
-          Time: '2001-08-24T00:00:00Z',
+          date: 20010824,
           ETime: '2001-08-24T01:00:00Z',
         },
         {
-          Time: '2001-08-25T00:00:00Z',
+          date: 20010825,
           ETime: '2001-08-25T01:00:00Z',
         },
       ];
       const range = [
-        { Time: '2001-08-22T00:00:00Z' },
-        { Time: '2001-08-23T00:00:00Z' },
+        { date: 20010822 },
+        { date: 20010823 },
       ];
       const data = filter(src, range);
       assert.typeOf(data, 'array', 'data should be an array');
@@ -53,7 +56,6 @@ describe('Util', () => {
         data,
         range,
       }));
-      assert.typeOf(data[0].date, 'date');
       assert.equal(data[0], src[1]);
       assert.equal(data[3], src[4]);
     });
