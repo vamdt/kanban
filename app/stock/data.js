151d504848701caa9b66d2c35154891112375b0d
Dejian Xu <xudejian2008@gmail.com>
2016-03-18 09:57:59 +0800

    separate event

--- app/stock/data.js
+++ app/stock/data.js
@@ -1,38 +1,4 @@
-class EventEmitter {
-  constructor() {
-    this.events = {};
-  }
-
-  on(event, cb) {
-    this.events[event] = this.events[event] || [];
-    if (this.events[event].indexOf(cb) < 0) {
-      this.events[event].push(cb);
-    }
-  }
-
-  off(event, cb) {
-    if (!this.events[event]) {
-      return;
-    }
-    if (!cb) {
-      delete this.events[event];
-      return;
-    }
-    const i = this.events[event].indexOf(cb);
-    if (i > -1) {
-      this.events[event].splice(i, 1);
-    }
-  }
-
-  emit(event, data) {
-    if (!this.events[event]) {
-      return;
-    }
-    this.events[event].forEach((fn) => {
-      fn(data);
-    });
-  }
-}
+import EventEmitter from './event';
 
 export default class IO extends EventEmitter {
   constructor() {
