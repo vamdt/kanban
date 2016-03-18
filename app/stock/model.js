7a2fe5505c87894ffea009b56be2444af0e1ffbd
Dejian Xu <xudejian2008@gmail.com>
2016-03-18 15:23:37 +0800

    define model

--- app/stock/model.js
+++ app/stock/model.js
@@ -1,11 +1,77 @@
 import EventEmitter from './event';
 
+class Typings {
+  constructor(name) {
+    Object.defineProperty(this, 'name', {
+      value: name.toLowerCase(),
+    });
+    const props = [
+      'Data',
+      'Line',
+    ];
+    props.forEach((name) => {
+      Object.defineProperty(this, name, {
+        value: [],
+      });
+    });
+  }
+}
+
+class Level {
+  constructor(name, prev) {
+    Object.defineProperty(this, 'name', {
+      value: name.toLowerCase(),
+    });
+
+    if (prev) {
+      Object.defineProperty(this, 'prev', {
+        value: prev.toLowerCase(),
+      });
+    }
+
+    Object.defineProperty(this, 'data', {
+      value: [],
+    });
+
+    const props = [
+      'Typing',
+      'Segment',
+      'Hub',
+    ];
+    props.forEach((name) => {
+      Object.defineProperty(this, name, {
+        value: new Typings(name),
+      });
+    });
+  }
+}
+
 export default class Model extends EventEmitter {
   constructor(id) {
     super();
     Object.defineProperty(this, 'id', {
-      enumerable: true,
       value: id,
+    });
+
+    Object.defineProperty(this, 'name', {
+      value: '',
+      writable: true,
+    });
+
+    const levels = [
+      'm1s',
+      'm5s',
+      'm30s',
+      'days',
+      'weeks',
+      'months',
+    ];
+    let prev = false;
+    levels.forEach((lname) => {
+      Object.defineProperty(this, lname, {
+        value: new Level(lname, prev),
+      });
+      prev = lname;
     });
   }
 }
--- test/model.test.js
+++ test/model.test.js
@@ -1,6 +1,71 @@
 import Model from '../app/stock/model';
 
 describe('Model', () => {
+  describe('property', () => {
+    const def_level = {
+      name: '',
+      data: [],
+      Typing: {
+        Data: [],
+        Line: [],
+      },
+      Segment: {
+        Data: [],
+        Line: [],
+      },
+      Hub: {
+        Data: [],
+        Line: [],
+      },
+    };
+    const defaults = {
+      id: '',
+      name: '',
+      m1s: def_level,
+      m5s: def_level,
+      m30s: def_level,
+      days: def_level,
+      weeks: def_level,
+      months: def_level,
+    };
+
+    const m = new Model('007');
+    const props = [
+      'id',
+      'name',
+    ];
+    it('should has expect property', () => {
+      Object.keys(defaults).forEach((k) => {
+        assert.property(m, k);
+        assert.typeOf(m[k], typeof defaults[k], k);
+      });
+    });
+
+    const levels = [
+      'm1s',
+      'm5s',
+      'm30s',
+      'days',
+      'weeks',
+      'months',
+    ];
+
+    describe('levels', () => {
+      it('should has expect property', () => {
+        levels.forEach((k) => {
+          Object.keys(def_level).forEach((p) => {
+            assert.property(m[k], p);
+            if (Array.isArray(def_level[p])) {
+              assert.isArray(m[k][p], `${k}.${p}`);
+            } else {
+              assert.typeOf(m[k][p], typeof def_level[p], `${k}.${p}`);
+            }
+          });
+        });
+      });
+    });
+  });
+
   describe('id', () => {
     it('should readonly', () => {
       const id = '007';
