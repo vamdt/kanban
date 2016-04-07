357622b018c05cba818ea43763f86e9011893b7d
Dejian Xu <xudejian2008@gmail.com>
2016-04-07 11:21:50 +0800

    remove arrow-parens set

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
